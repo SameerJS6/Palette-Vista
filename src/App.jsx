416dca205627ca4c3edee579e5028aa87c812ab0
SameerJS6 <sameersingh1234pok@gmail.com>
2023-05-30 21:45:40 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="mx-auto my-4 flex w-fit items-center justify-center gap-4 rounded-full bg-pink-50 px-8 py-4 text-2xl font-medium text-yellow-400">
+      Palette Vista
+    </div>
+  );
+}
